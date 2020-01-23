dfbd4cc03bb66f4e0c4842ea272cec01bf2f8476
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-23 15:06:35 +0000

    add attributes in book class

---
+++ lib/book.rb
@@ -0,0 +1,25 @@
+class Book
+  attr_accessor :name, :author, :rank, :amazon_url, :price, :page_count, :rating, :ratings_count
+
+  @@all = []
+
+  def intialize(list_info_hash)
+    add_attributes_from_hash(list_info_hash)
+    self.class.all << self
+  end
+
+  def add_amazon_attributes(amazon_attribute_hash)
+    add_attributes_from_hash(amazon_attribute_hash)
+  end
+
+  def add_attributes_from_hash(hash)
+    hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  def all
+    @@all
+  end
+
+end
