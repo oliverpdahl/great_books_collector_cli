e62d6a1e384f6b10a33b63b8248e8d0b9b9ba0f0
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-23 16:18:05 +0000

    set up the command line output for list level

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,9 +2,15 @@ class Scaper
 
   def self.scrape_list_page(list_url)
     doc = Nokogiri::HTML(open(list_url))
-    book_list_hash = {}
-    #scrape code
-    book_list_hash
+    book_cards = doc.css(#code to get to cards)
+    book_cards.collect do |book|
+      book_hash = {}
+      book_hash[:name] = book.css("").text #gets the name element
+      book_hash[:author] = book.css("").text #gets the location element
+      book_hash[:rank] = book.css("").text.to_i
+      book_hash[:amazon_url] = student.css("a").attribute("href").value #gets tt
+      book_hash
+    end
   end
 
   def self.scrape_amazon_page(amazon_url)
