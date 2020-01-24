095e1affa0093e4d0a3a1c1855fe774a2efe4c32
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-24 16:35:51 +0000

    scraping from great books working

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ class Scraper
 
   def self.scrape_list_page(list_url)
     doc = Nokogiri::HTML(open(list_url))
-    book_cards = doc.css("ol.list-unstyled li")#code to get to cards)
+    book_cards = doc.css("ol.list-unstyled li.item.pb-3")#code to get to cards)
     book_cards.collect do |book|
       book_hash = {}
       header = book.css("h4")
@@ -11,8 +11,7 @@ class Scraper
       book_hash[:name] = header.css("a").first.text.strip
       book_hash[:author] = header.css("a").last.text.strip
       book_hash[:rank] = /(?<=(\s))\d+(?=(\n\.))/.match(header_text).to_s.to_i
-      binding.pry
-      book_hash[:amazon_url] = student.css("a").attribute("href").value #gets tt
+      book_hash[:amazon_url] = book.css("div.pb-3 div.pull-left.mr-3 div a").attribute("href").value
       book_hash
     end
   end
