eef2a3c9c182b8806b1ec94ffda65eefd0dc1cc1
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-24 16:14:06 +0000

    rank, name, and author scraping correctly

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,15 @@ class Scraper
 
   def self.scrape_list_page(list_url)
     doc = Nokogiri::HTML(open(list_url))
-    book_cards = doc.css()#code to get to cards)
-    binding.pry
+    book_cards = doc.css("ol.list-unstyled li")#code to get to cards)
     book_cards.collect do |book|
       book_hash = {}
-      book_hash[:name] = book.css("").text #gets the name element
-      book_hash[:author] = book.css("").text #gets the location element
-      book_hash[:rank] = book.css("").text.to_i
+      header = book.css("h4")
+      header_text = header.text
+      book_hash[:name] = header.css("a").first.text.strip
+      book_hash[:author] = header.css("a").last.text.strip
+      book_hash[:rank] = /(?<=(\s))\d+(?=(\n\.))/.match(header_text).to_s.to_i
+      binding.pry
       book_hash[:amazon_url] = student.css("a").attribute("href").value #gets tt
       book_hash
     end
